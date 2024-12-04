4507ec84b27baebab91ac614935ae10ed3576292
Laraib Rizwan <161979255+Laraib2005@users.noreply.github.com>
2024-12-04 18:19:15 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <section className="about bg-cover bg-center text-white h-screen flex justify-start items-center px-5 sm:px-10" style={{ backgroundImage: "url(about.jpg)" }}>
-        <div className="about-content max-w-xl sm:max-w-2xl bg-black bg-opacity-60 p-8 rounded-lg text-left">
-          <h2 className="text-3xl sm:text-4xl font-bold mb-4">About Us</h2>
-          <p className="text-lg sm:text-xl mt-4">
-            Welcome to FOOT WEAR, where style meets comfort in every step. We are passionate about providing high-quality, durable, and fashionable shoes that suits all occasions. Whether you are looking for casual sneakers, elegant formal shoes, or sturdy boots, our diverse collection has something for everyone. 
+      <section 
+        className="about bg-cover bg-center text-white flex justify-start items-center px-5 sm:px-10" 
+        style={{ backgroundImage: "url(about.jpg)" }}
+      >
+        <div className="about-content max-w-full sm:max-w-2xl bg-black bg-opacity-60 p-4 sm:p-6 md:p-8 rounded-lg text-left">
+          <h2 className="text-xl sm:text-3xl md:text-4xl font-bold mb-4">About Us</h2>
+          <p className="text-sm sm:text-base md:text-lg mt-2 sm:mt-4">
+            Welcome to FOOT WEAR, where style meets comfort in every step. We are passionate about providing high-quality, durable, and fashionable shoes that suit all occasions. Whether you are looking for casual sneakers, elegant formal shoes, or sturdy boots, our diverse collection has something for everyone. 
             <br /><br />
             At FOOT WEAR, we believe that shoes are more than just a necessity; they are a statement of your unique personality and style. Our team is dedicated to sourcing the finest materials and delivering shoes that not only look great but feel amazing. 
             <br /><br />
@@ -18,4 +21,6 @@ const About = () => {
     </div>
   );
 };
+
 export default About;
+ 
